4499acd02e1172bf5728d472a14796c6785cc129
Giammarco Marinelli <Giam@Air-di-Vanessa.mshome.net>
2023-08-21 15:51:45 +0200

    Creato nuovo progetto

---
+++ src/main/java/com/example/restservicecomplete/RestservicecompleteApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicecomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicecompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicecompleteApplication.class, args);
+	}
+
+}
